97b69e004b990fbdc517fe388d299ffc0fcc8b28
Delight Sekhwela <delight.sekhwela@fnb.co.za>
2024-06-26 19:25:02 +0200

    AddLoad_Should_Succeed_WhenLoadDoesNotExceedsMaxPassengers

--- Elevator.Challenge/Elevator.Challenge.Tests/Domain/Elevator/ElevatorTests.cs
+++ Elevator.Challenge/Elevator.Challenge.Tests/Domain/Elevator/ElevatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using Elevator.Challenge.Domain.Elevator;
+using FluentAssertions;
 
 namespace Elevator.Challenge.Tests.Domain
 {
@@ -14,6 +15,21 @@ namespace Elevator.Challenge.Tests.Domain
             var exception = Assert.Throws<InvalidOperationException>(() => elevator.AddLoad(load));
 
             Assert.Equal("Exceeds maximum limit.", exception.Message);
+        }
+
+        [Fact]
+        public void AddLoad_Should_Succeed_WhenLoadDoesNotExceedsMaxPassengers()
+        {
+            int maxPassengers = 10;
+            int load = 6;
+            int currentPassengers = 0;
+            int passengerNumber = currentPassengers + load;
+
+            var elevator = new PassengerElevator(1, maxPassengers);
+
+            elevator.AddLoad(load);
+
+            elevator.PassengerNumber.Should().Be(passengerNumber);  
         }
     }
 }
